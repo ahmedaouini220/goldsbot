d61679083578b99a9d42460e7921343a28fe36a2
ahmedaouini220 <82116353+ahmedaouini220@users.noreply.github.com>
2022-11-06 14:31:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2439,7 +2439,6 @@ let embed_1 = new MessageEmbed()
 
 client.on('messageCreate', async message => {
   if(message.content.startsWith(prefix + 'info-map')) {
-     message.delete()
       if(!owner.includes(message.author.id)) return;
        let embed = new Discord.MessageEmbed()
       .setColor("#ff0000")
